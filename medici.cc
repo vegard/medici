dd08fa3c6aef7ae63f5975b8d4d840b151bbf3cd
Vegard Nossum <vegard.nossum@gmail.com>
2012-02-25 21:17:28 +0100

    Display leonardo sprite
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>

--- medici.cc
+++ medici.cc
@@ -1,8 +1,10 @@
 #include <cstdint>
 
 #include "dispcnt.hh"
+#include "obj.hh"
 
 #include "backgrounds/title.cc"
+#include "sprites/leonardo.cc"
 
 extern "C"
 int main(void)
@@ -11,18 +13,41 @@ int main(void)
 	dispcnt()
 		.blank(true);
 
-	/* Set palette */
+	/* Set BG palette */
 	for (unsigned int i = 0; i < title_palette_size; ++i)
 		*((volatile uint16_t *) 0x05000000 + i) = title_palette[i];
 
-	/* Copy background */
+	/* Copy BG data */
 	for (unsigned int i = 0; i < 240 * 160 / 4; ++i)
 		*((volatile uint32_t *) 0x06000000 + i) = title[i];
+
+	/* Set OBJ palette */
+	for (unsigned int i = 0; i < leonardo_palette_size; ++i)
+		*((volatile uint16_t *) 0x05000200 + i) = leonardo_palette[i];
+
+	/* Copy OBJ tiles */
+	for (unsigned int i = 0; i < 32 * 32 * 12 / 4; ++i)
+		*((volatile uint32_t *) 0x06014000 + i) = leonardo[i];
+
+	/* Clear OBJ attributes */
+	for (unsigned int i = 0; i < 128; ++i)
+		obj(i).enable(false);
+
+	/* Display leonardo sprite */
+	obj(0)
+		.tile(512)
+		.palette_256()
+		.size(2)
+		.x(200)
+		.y(80)
+		.enable(true);
 
 	/* Set BG mode */
 	dispcnt()
 		.mode(4)
-		.bg2(true);
+		.obj_mapping_1d(true)
+		.bg2(true)
+		.obj(true);
 
 	while (true)
 		;
