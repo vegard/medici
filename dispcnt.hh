d77eb2c484d876cd4e83aff39d37821c7dfe62e4
Vegard Nossum <vegard.nossum@gmail.com>
2012-02-25 21:15:43 +0100

    Add a few attributes to dispcnt
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>

--- dispcnt.hh
+++ dispcnt.hh
@@ -31,6 +31,12 @@ public:
 		return *this;
 	}
 
+	dispcnt &obj_mapping_1d(bool x)
+	{
+		value |= (uint16_t) x << 6;
+		return *this;
+	}
+
 	dispcnt &blank(bool x)
 	{
 		value |= (uint16_t) x << 7;
@@ -40,6 +46,12 @@ public:
 	dispcnt &bg2(bool x)
 	{
 		value |= (uint16_t) x << 10;
+		return *this;
+	}
+
+	dispcnt &obj(bool x)
+	{
+		value |= (uint16_t) x << 12;
 		return *this;
 	}
 };
